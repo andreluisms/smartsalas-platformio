9b4750a33a4bef44721257a54850ab6b50d9f5ad
Andre Lumesi <andreluis.ms@gmail.com>
2026-03-01 03:00:25 -0300

    Rearrumacao das pastas, adicao do codigo do atuador, sensor, controlador de sala

---
+++ sensor/src/main.cpp
@@ -0,0 +1,59 @@
+#include "Global.h"
+#include "Structs.h"
+#include "WiFiService.h"
+#include "Controller.h"
+#include "Config.h"
+
+#define RELE 19
+
+HardwareRecord hardware;
+WiFiService wiFiService;
+Controller controller;
+String master = "";
+
+bool SEND_DATA = false;
+String COMMAND;
+const uint16_t kIrLed = 12;
+IRsend irsend(kIrLed); 
+
+void setup() {
+
+	Serial.begin(115200);
+  pinMode(RELE, OUTPUT);
+	irsend.begin();
+	bool init = false;
+
+	wiFiService.connect();
+
+	do {
+		if ( controller.start(hardware) ) {
+			if ( controller.registerHardware(hardware) ) {
+				if(controller.getMaster(hardware, master))
+				{
+					Serial.print("master: ");
+					Serial.println(master);
+					init = true;
+				}
+				controller.setHardwareConfig(hardware);
+
+			}
+		}
+	} while( !init ); 
+
+	wiFiService.disconnect();
+
+    controller.setupBLEClient("ESP_ATUADOR", ATUADOR);  
+}
+
+void loop() {
+  
+  Serial.println("[Loop] Await message"); 
+  
+  if(SEND_DATA) {
+    Serial.println("[Loop] FOWARD TO SEND COMMAND"); 
+    controller.ExecuteCommand(COMMAND);
+    SEND_DATA = false;
+  }
+
+  delay(200);
+}
